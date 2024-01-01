b6de0db9f5c15ccd619ab987131fcacc981bdfe1
dzvinyangwi <editorofcode@gmail.com>
2024-01-01 18:00:46 +0100

    feat: :sparkles: Building a portfolio website

---
+++ venv/server.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect
+import json
+app = Flask(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_file(data):
+    with open('database.txt', 'a') as database:
+        database.write(f'{json.dumps(data)}\n')
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong, please try again'
+    
